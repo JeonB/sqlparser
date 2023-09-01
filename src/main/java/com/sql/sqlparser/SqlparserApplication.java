fe1161f23bf605afc6a1124cb8c804b2a820d11f
jeonb <JeonB@AkapsMacPro.local>
2023-09-01 14:22:38 +0900

    Initial commit

---
+++ src/main/java/com/sql/sqlparser/SqlparserApplication.java
@@ -0,0 +1,13 @@
+package com.sql.sqlparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlparserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlparserApplication.class, args);
+    }
+
+}
